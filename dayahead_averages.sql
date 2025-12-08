fdc0f80d4d22ce054400a7e9f5dfb94f2ad0860f
Diego Izaguirre <tenochca.di@outlook.com>
2025-12-07 23:43:37 -0600

    added price comparison between regions & dayhead region averages

---
+++ dayahead_averages.sql
@@ -0,0 +1,21 @@
+--Total price average for each region
+SELECT 
+    AVG(UES_Northwest) AS avg_nw,
+    AVG(UES_Siberia) AS avg_sb,
+    AVG(UES_Middle_Volga) AS avg_mv,
+    AVG(UES_Urals) AS avg_ur,
+    AVG(UES_Center) AS avg_c,
+    AVG(UES_South) AS avg_s
+FROM ues_dayahead_price;
+
+--Price averages per day
+SELECT 
+    substr(timestep, 1, 10) AS day,
+    AVG(UES_Northwest) AS avg_nw,
+    AVG(UES_Siberia) AS avg_sb,
+    AVG(UES_Middle_Volga) AS avg_mv,
+    AVG(UES_Urals) AS avg_ur,
+    AVG(UES_Center) AS avg_c,
+    AVG(UES_South) AS avg_s
+FROM ues_dayahead_price
+GROUP BY day;
---
+++ price_comparison.sql
@@ -0,0 +1,14 @@
+SELECT t.timestep, 'Center' AS region, d.UES_Center AS day_ahead, i.UES_Center AS intraday, ABS(i.UES_Center - d.UES_Center) AS diff
+FROM time_index_ues t
+JOIN ues_dayahead_price d ON d.time_id = t.id
+JOIN ues_intraday_price i ON i.time_id = t.id
+UNION ALL
+SELECT t.timestep, 'NorthWest', d.UES_Northwest, i.UES_Northwest, ABS(i.UES_Northwest - d.UES_Northwest) 
+FROM time_index_ues t
+JOIN ues_dayahead_price d ON d.time_id = t.id
+JOIN ues_intraday_price i ON i.time_id = t.id
+UNION ALL
+SELECT t.timestep, 'Sibera', d.UES_Siberia, i.UES_Siberia, ABS(i.UES_Siberia - d.UES_Siberia)
+FROM time_index_ues t
+JOIN ues_dayahead_price d ON d.time_id = t.id
+JOIN ues_intraday_price i ON i.time_id = t.id;
