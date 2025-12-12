69aa69135b15940376f5366b73b0b48764cee598
Diego Izaguirre <tenochca.di@outlook.com>
2025-12-12 00:44:13 -0600

    removed duplicates

--- intraday_averages.sql
+++
@@ -1,9 +0,0 @@
---intraday total price averages
-SELECT AVG(UES_Northwest) AS northwest_avg, AVG(UES_Siberia) AS siberia_avg, AVG(UES_Center) AS center_avg 
-FROM ues_intraday_price
-;
-
---intraday averages by day
-SELECT substr(timestep, 1, 10) as day, AVG(UES_Northwest) AS northwest_avg, AVG(UES_Siberia) AS siberia_avg, AVG(UES_Center) AS center_avg 
-FROM ues_intraday_price
-GROUP BY day;
--- pz_consumption_extremes.sql
+++
@@ -1,8 +0,0 @@
-SELECT substr(timestep,12,5) as hour, consumption_eur, price_eur FROM pz_dayahead_price_volume
-ORDER BY consumption_eur DESC
-LIMIT 10;
-;
-
-SELECT substr(timestep, 12, 5) as hour, consumption_sib, price_sib FROM pz_dayahead_price_volume
-ORDER BY consumption_sib DESC
-LIMIT 10;
